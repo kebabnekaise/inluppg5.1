52f4d3fa5a3ab06f04928228725cb275945ea48f
emsi06001 <emsi06001@E-PW0234N3>
2023-11-22 12:23:43 +0100

    Add project files.

---
+++ inluppg5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace inluppg4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv fem namn, med enter mellan dem");
+            string[] namn = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+
+            for(int i = 0;i < 5; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
